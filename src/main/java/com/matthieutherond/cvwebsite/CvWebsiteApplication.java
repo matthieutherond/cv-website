afd716b2c4dda3fcdcfd83ecc1fd0345fab9db67
matt <matthieu.therond@gmail.com>
2018-05-17 15:14:07 +0200

    Initial commit

---
+++ src/main/java/com/matthieutherond/cvwebsite/CvWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.matthieutherond.cvwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvWebsiteApplication.class, args);
+	}
+}
